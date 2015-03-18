dd9acd792b6ea690b7188f78ffaf9a89f4143538
zildjiean <piyapong-pp@hotmail.com>
2015-03-18 18:05:30 +0700

    Update breaknumber.py

--- breaknumber.py
+++ breaknumber.py
@@ -4,39 +4,52 @@ import urllib
 import re
 from cookielib import CookieJar
 
-#response = urllib.urlopen('http://s30000-102007-5ov.sipontum.hack.me/number.php') #change url when use hackme.
-#req = response.read()
-
 cj = CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-response = opener.open("http://s30000-102007-5ov.sipontum.hack.me/number.php")
-req = response.read()
+rnd = 0
 
-def getvalues(x):
-	text = re.search('(\-\d+|\d+), (\-\d+|\d+)\S+\s+\S+\s(\w+)',x)
-	result = text.group(1,2)
-	return result
 
-print getvalues(req)
+while (rnd < 60):
+	response = opener.open("http://s30000-102007-5ov.sipontum.hack.me/number.php")
+	req = response.read()
 
-def getcondition(x):
-	text = re.search('(maximum|minimum)',x)
-	result = text.group(1)
-	return result
 
-print getcondition(req)
+	def getvalues(x):
+		text = re.search('(\-\d+|\d+), (\-\d+|\d+)\S+\s+\S+\s(\w+)',x)
+		result = text.group(1,2)
+		return result
 
-def calculate(x,y):
-	if y == 'maximum':
-		x=max(x)
-	else :
-		x=min(x)
-	return x
+	print getvalues(req)
 
-print calculate(getvalues(req),getcondition(req))
+	def getscore(x):
+		text = re.search('Score:\s(\d+)\<\/p',x)
+		result = text.group(1)
+		return result
 
-formdata = { "number" : calculate(getvalues(req),getcondition(req)), "submit": "submit"}
-response = opener.open("http://s30000-102007-5ov.sipontum.hack.me/proc.php")
-req = response.read()
+	def getcondition(x):
+		text = re.search('(maximum|minimum)',x)
+		result = text.group(1)
+		return result
 
-print req
+	print getcondition(req)
+
+	def calculate(x,y):
+		if y == 'maximum':
+			x=max(x)
+		else :
+			x=min(x)
+		return x
+
+	print calculate(getvalues(req),getcondition(req))
+	number = calculate(getvalues(req),getcondition(req))
+
+	formdata = { "number" : number, "submit": "submit"}
+	data_encoded = urllib.urlencode(formdata)
+	response = opener.open("http://s30000-102007-5ov.sipontum.hack.me/proc.php",data_encoded)
+	req2 = response.read()
+
+
+	score = getscore(req)
+	print "Score : "+score
+
+	rnd=rnd+1
