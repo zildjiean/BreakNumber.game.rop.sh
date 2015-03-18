e71a5df40ec1cc406232341be66ca54c1d691ff3
zildjiean <piyapong-pp@hotmail.com>
2015-03-18 17:32:04 +0700

    Update breaknumber.py

--- breaknumber.py
+++ breaknumber.py
@@ -1,22 +1,42 @@
+#!/usr/bin/python
+import urllib2
 import urllib
 import re
+from cookielib import CookieJar
 
-response = urllib.urlopen('http://s30000-102007-m3r.sipontum.hack.me/number.php') #change url when use hackme.
+#response = urllib.urlopen('http://s30000-102007-5ov.sipontum.hack.me/number.php') #change url when use hackme.
+#req = response.read()
+
+cj = CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+response = opener.open("http://s30000-102007-5ov.sipontum.hack.me/number.php")
 req = response.read()
 
 def getvalues(x):
-	line = str(x)
 	text = re.search('(\-\d+|\d+), (\-\d+|\d+)\S+\s+\S+\s(\w+)',x)
-	result = text.group(1,2,3)
+	result = text.group(1,2)
 	return result
 
 print getvalues(req)
 
-def calculate(result):
-	if result[2] == 'maximum':
-		x=max(result)
+def getcondition(x):
+	text = re.search('(maximum|minimum)',x)
+	result = text.group(1)
+	return result
+
+print getcondition(req)
+
+def calculate(x,y):
+	if y == 'maximum':
+		x=max(x)
 	else :
-		x=min(result)
+		x=min(x)
 	return x
 
-print calculate(getvalues(req))
+print calculate(getvalues(req),getcondition(req))
+
+formdata = { "number" : calculate(getvalues(req),getcondition(req)), "submit": "submit"}
+response = opener.open("http://s30000-102007-5ov.sipontum.hack.me/proc.php")
+req = response.read()
+
+print req
